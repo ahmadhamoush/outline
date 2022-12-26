54376182d2f5e1a6e2926d34652f6dfac2d3e862
Ahmad Hamoush <hamoush@outlook.com>
2022-12-26 22:49:37 +0200

    Removed Swiping in Carousel

--- carousel.js
+++ carousel.js
@@ -1,10 +1,10 @@
-$(".carousel").swipe({
-excludedElements: "input, select, textarea, .noSwipe",
-swipeUp: function() {
-$(this).carousel('next');
-},
-swipeDown: function() {
-$(this).carousel('prev');
-},
-allowPageScroll: 'vertical'
-});
+// $(".carousel").swipe({
+// excludedElements: "input, select, textarea, .noSwipe",
+// swipeUp: function() {
+// $(this).carousel('next');
+// },
+// swipeDown: function() {
+// $(this).carousel('prev');
+// },
+// allowPageScroll: 'vertical'
+// });
