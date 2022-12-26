c65e27a964eac57a71332cc4dad3e8d674594158
Ahmad Hamoush <hamoush@outlook.com>
2022-12-27 01:59:54 +0200

    Added Back swiping on carousel

--- carousel.js
+++ carousel.js
@@ -1,10 +1,27 @@
-// $(".carousel").swipe({
-// excludedElements: "input, select, textarea, .noSwipe",
-// swipeUp: function() {
-// $(this).carousel('next');
-// },
-// swipeDown: function() {
-// $(this).carousel('prev');
-// },
-// allowPageScroll: 'vertical'
-// });
+$(".carousel").swipe({
+excludedElements: "input, select, textarea, .noSwipe",
+swipeUp: function() {
+$(this).carousel('next');
+},
+swipeDown: function() {
+$(this).carousel('prev');
+},
+allowPageScroll: 'vertical'
+});
+
+$(function(){
+  var carouselEl = $('.carousel');
+  var carouselItems = carouselEl.find('.item');
+  carouselEl.carousel({
+    interval: 2000
+  }).on('slid.bs.carousel', function (event) {
+    const index = carouselItems.siblings('.active').index();
+    if(index ==4){
+      $('.logo').css('filter','invert(1)')
+      $('.carousel-indicators').css('filter','invert(1)')
+    }else{
+        $('.logo').css('filter','invert(0)')
+            $('.carousel-indicators').css('filter','invert(0)')
+    }
+  })
+})
