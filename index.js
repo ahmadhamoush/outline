1ff4f07b3855da8316262a408a55b595e297ffe0
Ahmad Hamoush <hamoush@outlook.com>
2023-01-10 21:10:54 +0200

    Fixed structure

---
+++ index.js
@@ -0,0 +1,140 @@
+//Responsive Height based on mobile screen size
+const documentHeight = () => {
+  const doc = document.documentElement
+  doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
+}
+window.addEventListener('resize', documentHeight)
+documentHeight()
+
+//Splash screen
+setTimeout(() => {
+  document.querySelector('.splashScreen').style.backgroundColor = 'white'
+  document.querySelector('.logo-img').classList.toggle('toggle')
+  setTimeout(() => {
+    document.querySelector('.splashScreen').style.backgroundColor = 'black'
+    document.querySelector('.logo-img').classList.toggle('toggle')
+  }, 500)
+
+  setTimeout(() => {
+    document.querySelector('.splashScreen').style.background = 'linear-gradient(to left, white 50%, black 50%)';
+    document.querySelector('.splashScreen').style.backgroundSize = '200% 100%';
+    document.querySelector('.splashScreen').style.backgroundPosition = 'left';
+  }, 1000)
+  setTimeout(() => {
+    document.querySelector('.splashScreen').style.backgroundPosition = 'center';
+  }, 1500)
+  setTimeout(() => {
+    document.querySelector('.logo-img').src = './images/logo-mix.png'
+  }, 1800)
+})
+
+//close Navigation function
+function closeNav() {
+  document.querySelector('.menu-icon').checked = false;
+}
+
+
+let counter = 0;
+
+//show options container on click
+function showOptions(e) {
+  if (!e.target.classList.contains('left-arrow')) {
+    if (counter == 0) {
+      document.querySelector('.categoriesContainer').style.animationIterationCount = '1'
+      setTimeout(() => {
+        document.querySelector('.categoriesContainer').style.transform = 'translateX(0)'
+      }, 500)
+    } else {
+      document.querySelector('.optionsContainer').classList.toggle('showOptions')
+      document.querySelectorAll('.optionsContainer ul li').forEach(item => item.classList.add('animate'))
+    }
+    counter++;
+    console.log(counter)
+  }
+
+}
+
+var body = document.querySelector('.slide4');
+var except = document.querySelector('.optionsContainer');
+var except2 = document.querySelector('.categoriesContainer');
+
+//hiding options
+body.addEventListener("click", function(ev) {
+  except.classList.remove('showOptions')
+  except2.style.transform = 'translateX(-100%)'
+  counter = 0;
+  setTimeout(() => {
+    document.querySelector('.categoriesContainer').style.animationIterationCount = 'infinite'
+  }, 500)
+
+}, false);
+except.addEventListener("click", function(ev) {
+
+  ev.stopPropagation();
+}, false);
+except2.addEventListener("click", function(ev) {
+
+  ev.stopPropagation();
+}, false);
+
+
+
+//hide Categories function
+function hideCategories() {
+  counter = 0;
+  document.querySelector('.categoriesContainer').style.transform = 'translateX(-100%)'
+  document.querySelector('.optionsContainer').classList.remove('showOptions')
+  setTimeout(() => {
+    document.querySelector('.categoriesContainer').style.animationIterationCount = 'infinite'
+  }, 500)
+
+}
+
+
+//Detect swipe on menu container
+let touchstartX = 0
+let touchendX = 0
+
+function checkDirection() {
+  if (touchendX < touchstartX) alert('swiped left!')
+  if (touchendX > touchstartX) alert('swiped right!')
+}
+
+document.querySelector('.menu-right').addEventListener('touchstart', e => {
+  touchstartX = e.changedTouches[0].screenX
+})
+
+document.querySelector('.menu-right').addEventListener('touchend', e => {
+  touchendX = e.changedTouches[0].screenX
+  checkDirection()
+})
+
+// -- Google Maps --
+
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+
+  const uluru = {
+    lat: 33.89494103856613,
+    lng: 35.5127938237086
+  };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 19,
+    center: uluru,
+    backgroundColor: 'black',
+    gestureHandling: 'greedy',
+    navigation: {
+      mouseWheelZoomEnabled: false,
+      browserTouchPanEnabled: false
+    },
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
+
+window.initMap = initMap;
